253a539e0adb3273f6ad0a2bb4dadeb582a629d0
Sam F-Harrison <sgfletcherharrison@gmail.com>
2026-02-12 22:13:44 +0000

    feat: add checkbox

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { Checkbox } from '@/components/ui/checkbox';
 import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table';
 import { H1, H2, H3, P } from '@/components/ui/typography';
 import { ArrowUpIcon, Plus } from 'lucide-react';
@@ -57,15 +58,42 @@ export default function Page() {
             <Plus />
           </Button>
         </div>
-        <Card className="p-0">
+        <Card className="py-0 px-2">
           <Table>
             <TableBody>
               <TableRow>
-                <TableCell className="px-4">
+                <TableCell>
                   <P>Work</P>
                   <P isSubtext>Monthly</P>
                 </TableCell>
-                <TableCell className="px-4 text-right">
+                <TableCell className="text-right">
+                  <H3>£2,120</H3>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Card>
+      </div>
+
+      <div className="px-4">
+        <div className="flex justify-between pl-2 pb-2 items-end">
+          <H3>Income</H3>
+          <Button variant="outline" size="icon-lg">
+            <Plus />
+          </Button>
+        </div>
+        <Card className="py-0 px-2">
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell className="w-9">
+                  <Checkbox />
+                </TableCell>
+                <TableCell>
+                  <P>Work</P>
+                  <P isSubtext>Monthly</P>
+                </TableCell>
+                <TableCell className="text-right">
                   <H3>£2,120</H3>
                 </TableCell>
               </TableRow>
